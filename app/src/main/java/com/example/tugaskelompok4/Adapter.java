f6a92e5f118842ee61bc35784a2866f3ca771f0d
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-08-18 19:49:37 +0700

    feat : Adding adapter for displaying the response

---
+++ app/src/main/java/com/example/tugaskelompok4/Adapter.java
@@ -0,0 +1,12 @@
+package com.example.tugaskelompok4;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterProject extends RecyclerView.Adapter<> {
+
+
+    class ViewHolderProject
+}
