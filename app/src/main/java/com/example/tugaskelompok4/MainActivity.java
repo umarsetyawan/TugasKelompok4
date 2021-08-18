bf7c4261457dee172c6b0b0680ce0a9b94ad3842
Umar Setyawan <umarsetyawan10@gmail.com>
2021-08-18 19:27:16 +0700

    Feat : Added getData object

--- app/src/main/java/com/example/tugaskelompok4/MainActivity.java
+++ app/src/main/java/com/example/tugaskelompok4/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getData();
     }
+
+    private void getData()
+    {
+        AndroidNetworking.get("https://reqres.in/api/users?page=2")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // do anything with response
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                    }
+                });
+
+    }
+
 }
