c715334ed9a4051ff92f717829411d189ef053d0
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-08-19 04:33:41 +0700

    fix: repaired MainActivity and adding image getter in Adapter

--- app/src/main/java/com/example/tugaskelompok4/Adapter.java
+++ app/src/main/java/com/example/tugaskelompok4/Adapter.java
@@ -1,6 +1,8 @@
 package com.example.tugaskelompok4;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,11 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.BitmapRequestListener;
 
 import java.util.List;
 
@@ -30,6 +37,25 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.email.setText(users.get(position).getEmail());
         holder.name.setText(users.get(position).getFirstName() + " " + users.get(position).getLastName());
+        AndroidNetworking.get(users.get(position)
+                .getAvatar())
+                .setTag("imageRequestTag")
+                .setPriority(Priority.MEDIUM)
+                .setBitmapMaxHeight(400)
+                .setBitmapMaxWidth(400)
+                .setBitmapConfig(Bitmap.Config.ARGB_8888)
+                .build()
+                .getAsBitmap(new BitmapRequestListener() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        holder.avatar.setImageBitmap(response);
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+
+                    }
+                });
     }
 
     @Override
