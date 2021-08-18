2f115fed152a04101b729e07762b23278f9b1ea3
Umar Setyawan <umarsetyawan10@gmail.com>
2021-08-18 20:19:56 +0700

    feat : ganti ke jsonobject

--- app/src/main/java/com/example/tugaskelompok4/MainActivity.java
+++ app/src/main/java/com/example/tugaskelompok4/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tugaskelompok4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -8,15 +10,25 @@ import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    Adapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.rvdata);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new Adapter(this, User);
 
         getData();
     }
@@ -26,14 +38,15 @@ public class MainActivity extends AppCompatActivity {
         AndroidNetworking.get("https://reqres.in/api/users?page=2")
                 .setPriority(Priority.LOW)
                 .build()
-                .getAsJSONArray(new JSONArrayRequestListener() {
+                .getAsJSONObject(new JSONObjectRequestListener() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                        // do anything with response
+                    public void onResponse(JSONObject response) {
+                        response.get("")
                     }
+
                     @Override
-                    public void onError(ANError error) {
-                        // handle error
+                    public void onError(ANError anError) {
+
                     }
                 });
 
