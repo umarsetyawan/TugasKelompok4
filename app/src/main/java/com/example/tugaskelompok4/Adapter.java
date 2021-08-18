dbbba84a74e9e1f3f75b115624db8c3e8a4b2739
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2021-08-18 20:01:01 +0700

    fix: repairing adapter and adding view holder layout

--- app/src/main/java/com/example/tugaskelompok4/Adapter.java
+++ app/src/main/java/com/example/tugaskelompok4/Adapter.java
@@ -1,12 +1,49 @@
 package com.example.tugaskelompok4;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterProject extends RecyclerView.Adapter<> {
+import java.util.List;
 
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
-    class ViewHolderProject
+    private final List<User> users;
+
+    public Adapter(List<User> users) {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder, parent));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.email.setText(users.get(position).getEmail());
+        holder.name.setText(users.get(position).getFirstName() + " " + users.get(position).getLastName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (users == null) ? 0 : users.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView email, name;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            email = itemView.findViewById(R.id.email);
+            name = itemView.findViewById(R.id.name);
+        }
+    }
 }
